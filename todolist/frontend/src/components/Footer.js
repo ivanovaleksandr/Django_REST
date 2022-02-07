a908ed6a084c4405e8ac94b04590b6c6a7e1a457
ivanovaleksandr <70534387+ivanovaleksandr@users.noreply.github.com>
2022-02-07 20:24:39 +0300

    Update Footer.js

--- todolist/frontend/src/components/Footer.js
+++ todolist/frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Footer = () => {
     return (
         <footer className='footer-copyright fixed-bottom text-center bg-light py-3'>
-            &copy; {new Date().getFullYear()} Copyright: <a className='text-decoration-none' href='https://www.linkedin.com/in/anatolytsi'> Anatolii Tsirkunenko </a>
+            &copy; {new Date().getFullYear()} Copyright: <a className='text-decoration-none' href='https://www.linkedin.com/in/anatolytsi'> Ivanov Aleksandr </a>
         </footer>
     )
 }
