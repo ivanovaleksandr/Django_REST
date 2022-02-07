c94d1f415ddbf9def58dc18c6b3fbbe1c554898e
Александр <inter06@.mail.ru>
2022-02-07 22:11:42 +0300

    Урок 10

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'drf_yasg',
+    'graphene_django',
 
     # custom apps
     'users',
@@ -165,4 +166,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.AcceptHeaderVersioning',
+}
+
+GRAPHENE = {
+    'SCHEMA': 'todo_notes.schema.schema',
 }
